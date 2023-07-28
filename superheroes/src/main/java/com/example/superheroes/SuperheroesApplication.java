d23fd23ac1c7fd65175a1ae4b0866e623cbfc1aa
Neelkanth7 <neelkanthtripathi7@gmail.com>
2023-07-28 20:39:39 +0100

    Finished QL6

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,28 +1,18 @@
 package com.example.superheroes;
 
-import com.example.superheroes.model.Hero;
-import com.example.superheroes.service.HeroService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner{
+public class SuperheroesApplication{
 
-	@Autowired
-	HeroService heroService;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
-		for (Hero hero : heroes){
-			System.out.println(hero);
-		}
-	}
+
+
 }
