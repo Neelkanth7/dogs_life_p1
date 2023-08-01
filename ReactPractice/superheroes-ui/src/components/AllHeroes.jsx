f8f14c26c8576fc8e6a1db403ad5ff5259fe42b1
Neelkanth7 <neelkanthtripathi7@gmail.com>
2023-08-01 18:53:29 +0100

    QL6 completed

--- ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
+++ ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import HeroDetail from './HeroDetail'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { useState } from 'react'
 
 const AllHeroes = () => {
+
+  const[total, setTotal] = useState(0);
+
+  const totalCountHandler = (name) =>{
+    console.log(name)
+    setTotal(total+1);
+}
 
   const heroes =
     [
@@ -20,13 +27,16 @@ const AllHeroes = () => {
     ]
 
   return (
-    <Row>
-      {heroes.map(hero => (
-        <div className='container' key={hero.id}>
-          <HeroDetail info={hero} />
-        </div>
-      ))}
+    <>
+    <h1> Total count is: {total}</h1>
+    <Row >
+    {heroes.map(hero => (
+      <div className='container' key={hero.id.toString()}>
+        <HeroDetail info={hero} parentCount={totalCountHandler}/>
+      </div>
+     ))}
     </Row>
+  </>
   )
 }
 
